306914bbdd11e5d417b5ab099b3a37e82a741551
mersad <mersad.up@gmail.com>
2024-03-18 19:14:37 +0330

    deploy test

--- actions/authActions.tsx
+++ actions/authActions.tsx
@@ -129,11 +129,15 @@ export async function signInAction(formData: FormData) {
 
     const prisma = new PrismaClient();
 
+    console.log(prisma);
+
     const user = await prisma.user.findUnique({
       where: {
         username,
       },
     });
+
+    console.log(user);
 
     if (!user) {
       return JSON.parse(
@@ -146,6 +150,8 @@ export async function signInAction(formData: FormData) {
 
     const isTruePassword = checkPassword(password, user.password); // Check password
 
+    console.log(isTruePassword);
+
     if (!isTruePassword) {
       return JSON.parse(
         JSON.stringify({
@@ -156,6 +162,8 @@ export async function signInAction(formData: FormData) {
     }
 
     const token = generateToken({ username }); // Generate token
+
+    console.log(token);
 
     cookies().set({
       name: "token",
