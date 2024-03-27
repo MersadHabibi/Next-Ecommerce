f5a432d20a3b9c2a7f01917c164001055ed0e4ef
mersad <mersad.up@gmail.com>
2024-03-27 11:21:02 +0330

    refactor dependencies & swiper

--- components/templates/Index/NewestProducts/NewestProductsSwiper.tsx
+++ components/templates/Index/NewestProducts/NewestProductsSwiper.tsx
@@ -1,55 +1,29 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
 import ProductCard from "@/components/modules/ProductCard";
 
-import { Autoplay } from "swiper/modules";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 export default function NewestProductsSwiper() {
   return (
-    <Swiper
-      slidesPerView={1}
-      // centeredSlides={true}
-      loop={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: true,
+    <Carousel
+      opts={{
+        align: "start",
       }}
-      breakpoints={{
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 4,
-        },
-      }}
-      spaceBetween={20}
-      pagination={{
-        type: "fraction",
-      }}
-      modules={[Autoplay]}
-      // navigation={true}
-      className="mySwiper">
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard />
-      </SwiperSlide>
-    </Swiper>
+      className="w-full">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
+            <ProductCard />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      {/* <CarouselPrevious /> */}
+      {/* <CarouselNext /> */}
+    </Carousel>
   );
 }
